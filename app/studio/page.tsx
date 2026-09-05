91644ab7ba9149f0cb984068b52d50b4b99272b8
Ai FH Systems <ashleyfordhibberd@gmail.com>
2026-09-05 11:39:48 +0100

    Add fifteen optional Florist OS studio appearances

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -1,9 +1,11 @@
 import StudioClient from './studio-client';
+import StudioAppearance from './studio-appearance';
 import StudioLoginForm from './login-form';
 import { hasStudioSession } from '@/lib/studio-auth';
 
 export const dynamic = 'force-dynamic';
 
 export default async function StudioPage() {
-  return await hasStudioSession() ? <StudioClient /> : <StudioLoginForm />;
+  return await hasStudioSession() ? <StudioAppearance><StudioClient /></StudioAppearance> : <StudioLoginForm />;
 }
+
